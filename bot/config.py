3812abaedba350e546cf579f2ae51254c3a6b786
Animepahe498 <148706595+Animepahe498@users.noreply.github.com>
2023-10-27 12:39:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -26,7 +26,7 @@ class Var:
     # Database Credentials
 
     REDIS_URI = config("REDIS_URI", default="http://redis-11976.c74.us-east-1-4.ec2.cloud.redislabs.com:11976")
-    REDIS_PASS = config("REDIS_PASSWORD", default=wqZTRNayJ6PMyMqsWCbgSVGVH0GKSW5B)
+    REDIS_PASS = config("REDIS_PASSWORD", default="wqZTRNayJ6PMyMqsWCbgSVGVH0GKSW5B")
 
     # Channels Ids
 
